ea08917e1a1aa1521b975ae14b2904eb82c240fb
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2018-04-22 08:31:02 -0500

    added data for props

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,27 @@
+import React, { Component } from 'react';
 import './index.css';
+import data from '../data/data.json';
 import Card from './Card';
 
-export default () => (
-  <div className="App">
-    <header className="App-header">
-      <img src="/static/logo.png" className="static-logo" alt="logo" />
-    </header>
-    <div className="Grid">
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  </div>
-);
+export default class Index extends Component {
+  static async getInitialProps() {
+    return { cards: data };
+  }
+
+  render() {
+    const { cards } = this.props;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="/static/logo.png" className="static-logo" alt="logo" />
+        </header>
+        <div className="Grid">
+          {
+            cards.map(card => <Card key={card.id} />)
+          }
+        </div>
+      </div>
+    );
+  }
+}
