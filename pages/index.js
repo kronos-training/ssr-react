f6ca77ae49e7729b89e4af1865b2207e317074ca
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2018-04-22 08:16:23 -0500

    added initial components from template

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
-export default () => {
-return (<h1>Hello world with Next.js</h1>);
-}
+import './index.css';
+import Card from './Card';
+
+export default () => (
+  <div className="App">
+    <header className="App-header">
+      <img src="/static/logo.png" className="static-logo" alt="logo" />
+    </header>
+    <div className="Grid">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  </div>
+);
