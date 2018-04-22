a7535517d18ef67279af57b7f7f553628ed41a36
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2018-04-22 08:42:36 -0500

    added redux actions

--- pages/store.js
+++ pages/store.js
@@ -8,6 +8,22 @@ const initialState = {
   cards: []
 };
 
+// Actions
+export const initialCards = () => {
+  return {
+    type: 'INITIAL_CARDS',
+    cards: data
+  };
+}
+
+export const addItem = (item) => {
+  return {
+    type: 'ADD_ITEM',
+    item
+  };
+}
+
+
 export const store = (initialState) => {
   return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunkMiddleware)));
 }
